b5ca8353f8866d211a417e496aa3d73eede6bc2b
LukeVerlaan <luke-verlaan@hotmail.com>
2020-04-28 12:03:18 +0200

    Info Page Content
    Little work on info page content.

--- SmartHotel360-Mobile-master/Source/SmartHotel.Clients/SmartHotel.Clients/ViewModels/InfoViewModel.cs
+++ SmartHotel360-Mobile-master/Source/SmartHotel.Clients/SmartHotel.Clients/ViewModels/InfoViewModel.cs
@@ -2,10 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace SmartHotel.Clients.Core.ViewModels
 {
-    class InfoViewModel : ViewModelBase
+    public class InfoViewModel : ViewModelBase
     {
+        public ICommand SendToWebsite => new Command<string>(OpenBrowser);
+
+        public InfoViewModel()
+        {
+
+        }
+
+        private void OpenBrowser(string url)
+        {
+            Launcher.OpenAsync(new Uri(url));
+        }
     }
+
+    
 }
